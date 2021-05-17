89db55bd7b0f6c4fc05a2d7239fdd7ddb01fbb6a
starboyalankarartist <alankarartist@gmail.com>
2021-05-17 19:04:20 +0530

    AlFileUnlatcher

--- AlFileUnlatcher.py
+++ AlFileUnlatcher.py
@@ -2,6 +2,7 @@ import subprocess, re, os
 from tkinter import*
 from tkinter import font
 import pyttsx3
+from PIL import ImageTk, Image
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 
@@ -9,11 +10,26 @@ class AlFileUnlatcher():
     
     def __init__(self):
         root = Tk(className = " ALFILEUNLATCHER ")
-        root.geometry("400x125+1500+890")
+        root.geometry("400x175+1500+840")
         root.resizable(0,0)
         root.iconbitmap(os.path.join(cwd+'\\UI\\icons', 'alfileunlatcher.ico'))
         root.config(bg="#ffe69b")
+        root.overrideredirect(1)
         color = '#ffe69b'
+
+        def callback(event):
+            root.geometry("360x100+1550+915")
+
+        def showScreen(event):
+            root.deiconify()
+            root.overrideredirect(1)
+
+        def screenAppear(event):
+            root.overrideredirect(1)
+
+        def hideScreen():
+            root.overrideredirect(0)
+            root.iconify()
 
         def speak(audio):
             engine = pyttsx3.init('sapi5')
@@ -69,24 +85,49 @@ class AlFileUnlatcher():
                         text.insert(1.0, 'Set a default application to open the input file')
                         speak('Set a default application to open the input file')
 
-        appHighlightFont = font.Font(family='sans-serif', size=12, weight='bold')
-        textHighlightFont = font.Font(family='Segoe UI', size=12, weight='bold')
+        textHighlightFont = font.Font(family='OnePlus Sans Display', size=12)
+        appHighlightFont = font.Font(family='OnePlus Sans Display', size=12, weight='bold')
+
+        #title bar
+        titleBar = Frame(root, bg='#141414', relief=SUNKEN, bd=0)
+        icon = Image.open(os.path.join(cwd+'\\UI\\icons', 'alfileunlatcher.ico'))
+        icon = icon.resize((30,30), Image.ANTIALIAS)
+        icon = ImageTk.PhotoImage(icon)
+        iconLabel = Label(titleBar, image=icon)
+        iconLabel.photo = icon
+        iconLabel.config(bg='#141414')
+        iconLabel.grid(row=0,column=0,sticky="nsew")
+        titleLabel = Label(titleBar, text='ALFILEUNLATCHER', fg='#909090', bg='#141414', font=appHighlightFont)
+        titleLabel.grid(row=0,column=1,sticky="nsew")
+        closeButton = Button(titleBar, text="x", bg='#141414', fg="#909090", borderwidth=0, command=root.destroy, font=appHighlightFont)
+        closeButton.grid(row=0,column=3,sticky="nsew")
+        minimizeButton = Button(titleBar, text="-", bg='#141414', fg="#909090", borderwidth=0, command=hideScreen, font=appHighlightFont)
+        minimizeButton.grid(row=0,column=2,sticky="nsew")
+        titleBar.grid_columnconfigure(0,weight=1)
+        titleBar.grid_columnconfigure(1,weight=10)
+        titleBar.grid_columnconfigure(2,weight=1)
+        titleBar.grid_columnconfigure(3,weight=1)
+        titleBar.pack(fill=X)
 
         #file widget
         fileText = Label(root, text="FILE TO BE SEARCHED AND OPENED")
         fileText.pack()
-        fileText.config(bg=color,fg="#0078d7",font=textHighlightFont)
-        fileText= Entry(root, bg="#0078d7", fg='white', highlightbackground=color, highlightcolor=color, highlightthickness=3, bd=0,font=appHighlightFont)
+        fileText.config(bg=color,fg="#0078d7",font=appHighlightFont)
+        fileText= Entry(root, bg="#0078d7", fg='white', highlightbackground=color, highlightcolor=color, highlightthickness=3, bd=0,font=textHighlightFont)
         fileText.pack(fill=X)
 
         #submit button
         find = Button(root, borderwidth=0, highlightthickness=3, text="OPEN FILE", command=find)
-        find.config(bg=color,fg="#0078d7",font=textHighlightFont)
+        find.config(bg=color,fg="#0078d7",font=appHighlightFont)
         find.pack(fill=X)
 
         text = Text(root, font="sans-serif",  relief=SUNKEN , highlightbackground=color, highlightcolor=color, highlightthickness=5, bd=0)
-        text.config(bg="#0078d7", fg='white', height=2, font=appHighlightFont)
+        text.config(bg="#0078d7", fg='white', height=2, font=textHighlightFont)
         text.pack(fill=BOTH, expand=True)
+
+        titleBar.bind("<B1-Motion>", callback)
+        titleBar.bind("<Button-3>", showScreen)
+        titleBar.bind("<Map>", screenAppear)
 
         root.mainloop()
 
